6bb9068bbefb09c03fa5d7bba2c675180c9320b1
shka03 <katsurashigeki0325@gmail.com>
2023-10-11 12:02:16 +0900

    ルーティングの設定追加完了。

--- Backend_Project/Framework_practice/laravel/CookieBakery/routes/web.php
+++ Backend_Project/Framework_practice/laravel/CookieBakery/routes/web.php
@@ -10,9 +10,31 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+| example code
+|  Route::get('/', function () {
+|    return view('welcome');
+|  });
 */
+    Route::get('/', function () {
+        return view('home');
+    })->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
+
+    Route::get('/menu', function () {
+        return view('menu');
+    })->name('menu');
+
+    Route::get('/location', function () {
+        return view('location');
+    })->name('location');
+
+    Route::get('/history', function () {
+        return view('history');
+    })->name('history');
+
+    Route::get('/contact', function () {
+        return view('contact');
+    })->name('contact');
